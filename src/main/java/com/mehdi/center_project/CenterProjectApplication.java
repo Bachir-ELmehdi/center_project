91f72f81ba2bf95d042f7b1f7c04c803c5b4e984
Bachir-ELmehdi <bachirelmehdii@gmail.com>
2021-03-27 22:35:36 +0100

    first commit

---
+++ src/main/java/com/mehdi/center_project/CenterProjectApplication.java
@@ -0,0 +1,13 @@
+package com.mehdi.center_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CenterProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CenterProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mehdi/center_project/CenterProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mehdi.center_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CenterProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
